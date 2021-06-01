d984c8518121344bb7b647115da172a84292ee58
Alka singh <alkasingh6feb@gmail.com>
2021-06-01 18:04:43 +0530

    show list student
    Signed-off-by: Alka singh <alkasingh6feb@gmail.com>

--- resources/views/homework/_archieve.blade.php
+++ resources/views/homework/_archieve.blade.php
@@ -1,54 +1,203 @@
 <div class="tab-pane" role="tabpanel" id="menu3">
     <div class="card shadow mt-3">
-      <div
-        class="
-          card-body card_left_border
-          border_left_color4
-        "
-      >
-        <div class="subject_edit">
-          <div class="subject_practis">
-            <p class="subject">Math Practice ssssss</p>
-            <p class="time_pera">
-              <span>23 Jan,</span
-              ><span>04:00pm - 05:00pm</span>
-            </p>
-          </div>
-          <div class="starttime_block">
-            <p class="sub_subject">Starts in</p>
-            <p class="green_text">00:08:55</p>
-          </div>
-          <div class="class_block">
-            <p class="sub_subject">Class</p>
-            <p class="blue_text">10th</p>
-          </div>
-          <div class="class_block">
-            <p class="sub_subject">Subject</p>
-            <p class="blue_text">Maths</p>
-          </div>
-          <div class="class_block">
-            <p class="sub_subject">Time</p>
-            <p class="blue_text">30 Min</p>
-          </div>
-          <div class="class_mb">
-            <p class="sub_subject">Students</p>
-            <p class="blue_text">30</p>
-          </div>
-          <div class="delite_icon">
-            <img
-              src="{{ asset('wa/dashboard/homework/img/delete.svg')}}"
-              width="20"
-              height="20"
-            />
-          </div>
-          <div class="edit_icon">
-            <img
-              src="{{ asset('wa/dashboard/homework/img/edit.svg')}}"
-              width="20"
-              height="20"
-            />
-          </div>
-        </div>
+{{--  --}}
+<div class="card">
+  <div class="session_data_block archive">
+    <div class=" archiv_block">
+      <div>
+        <p class="session_title">Session Date</p>
+        <p class="session_date">{{ $session->start_date_time->format('d/m/y')}}</p>
+      </div>
+      <div>
+        <p class="session_title">Session Time</p>
+        <p class="session_date">
+          {{ $session->start_date_time->format('h:i:A')}}
+        </p>
+      </div>
+      <div>
+        <p class="session_title">Teachers Name</p>
+        <p class="session_date">{{$session->batch->assignteacher->name}}</p>
+      </div>
+      <div>
+        <p class="session_title">Class</p>
+        <p class="session_date">{{$session->batch->classmaster->name}}</p>
       </div>
     </div>
   </div>
+  <div class="card-body">
+    <div class="d-flex justify-content-between">
+      <div>
+        <p class="session_no">Archive</p>
+      </div>
+    
+    </div>
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="nav-item">
+        <a class="nav-link home_work_tab active" data-toggle="tab" href="#homework">Assigned
+          homework</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link home_work_tab" data-toggle="tab" href="#quiz">Assigned Quiz</a>
+      </li>
+    </ul>
+
+    <!-- Tab panes -->
+    <div class="tab-content">
+      {{-- start foreach --}}
+      @foreach($sessions as $session)
+      <div id="homework" class="tab-pane active">
+        <br>
+        <section class="">
+          <div class="">
+            <div class="row">
+              <div class="col-md-12">
+                <div id="accordion">
+                  <div class="card mb-3">
+                    <div class="card-header">
+                      <a class="card-link text-dark" data-toggle="collapse" href="#collapseOne"><span class="float-right"></span>
+                        <!-- <h6>Collapsible Group Item #1</h6> -->
+                        <div class="accordian_card_display">
+                          <div>1.</div>
+                          <div>Session Name</div>
+                          <div>12 April 2021</div>
+                          <div>Class 9</div>
+                          <div>4 Students</div>
+                          <div>02 Submission</div>
+                        </div>
+                      </a>
+                    </div>
+                    <div id="collapseOne" class="collapse show" data-parent="#accordion">
+                      <div class="card-body">
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/inactive-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/active-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/inactive-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/active-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card mb-3">
+                    <div class="card-header">
+                      <a class="
+                            collapsed
+                            card-link
+                            text-dark
+                          " data-toggle="collapse" href="#collapseTwo"><span class="float-right"></span>
+                          <div class="accordian_card_display">
+                            <div>2.</div>
+                            <div>Session Name</div>
+                            <div>12 April 2021</div>
+                            <div>Class 9</div>
+                            <div>4 Students</div>
+                            <div>02 Submission</div>
+                          </div>
+                      </a>
+                    </div>
+                    <div id="collapseTwo" class="collapse" data-parent="#accordion">
+                      <div class="card-body">
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/inactive-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/active-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/inactive-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/active-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card mb-3">
+                    <div class="card-header">
+                      <a class="
+                            collapsed
+                            card-link
+                            text-dark
+                          " data-toggle="collapse" href="#collapseThree"><span class="float-right"></span>
+                          <div class="accordian_card_display">
+                            <div>3.</div>
+                            <div>Session Name</div>
+                            <div>12 April 2021</div>
+                            <div>Class 9</div>
+                            <div>4 Students</div>
+                            <div>02 Submission</div>
+                          </div>
+                        <!-- <span class="float-right"><i class="ti-plus"></i></span> -->
+                      </a>
+                    </div>
+                    <div id="collapseThree" class="collapse" data-parent="#accordion">
+                      <div class="card-body">
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/inactive-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/active-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/inactive-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                        <div class="accordian_card_inner_block">
+                          <div class="inner_nameblock">David Phillipe </div>
+                          <div class="inner_check_block"><img src="{{ asset('wa/teacherdashboard/img/active-check.svg')}}"></div>
+                          <div class="inner_reminder_block"><a href="#">Send Reminder</a></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      {{-- end foreach --}}
+      <div id="quiz" class="container tab-pane fade">
+        <br>
+        <h3>Menu 1</h3>
+        <p>
+          Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+    <hr>
+  </div>
+</div>
+{{--  --}}
+      </div>
+    </div>
