58144e859a9406ef589b1eb0428f4bbb8f1b3c86
Alka singh <alkasingh6feb@gmail.com>
2021-06-16 17:42:24 +0530

    update for pdf
    Signed-off-by: Alka singh <alkasingh6feb@gmail.com>

--- resources/views/homework/_homework_details.blade.php
+++ resources/views/homework/_homework_details.blade.php
@@ -57,7 +57,7 @@
                               
                               @foreach($homeworkContent as $hwcontent)
                               <div class="swiper-slide swiper-slide-visible swiper-slide-next" role="group" aria-label="{{ $loop->iteration }} / {{ $loop->count }}" style="width: 204.5px; margin-right: 10px;">
-                                <img src="{{ url("storage/homeworks".$assignedHomework->id."/".$hwcontent->image_path) }}#toolbar=0" class="">
+                                <img src="{{ url("storage/homeworks/".$hwcontent->image_path) }}#toolbar=0" class="">
                                 </div>
                               
                               @endforeach
@@ -95,9 +95,9 @@
                           <div class="border-section second-border-section">
                             <div> 
                             @if($multiple)
-                                 <embed src="{{ url("storage/homeworks".$assignedHomework->id."/".$homeworkContent->first()->image_path) }}#toolbar=0" type="application/pdf" width="600" height="400" />
+                                 <embed src="{{ url("storage/homeworks/".$homeworkContent->first()->image_path) }}#toolbar=0" type="application/pdf" width="600" height="400" />
                             @else
-                                 <embed src="{{ url("storage/homeworks".$assignedHomework->id."/".$homeworkContent->image_path) }}#toolbar=0" type="application/pdf" width="600" height="400" />
+                                 <embed src="{{ url("storage/homeworks/".$homeworkContent->image_path) }}#toolbar=0" type="application/pdf" width="600" height="400" />
                             
                             @endif
                              
